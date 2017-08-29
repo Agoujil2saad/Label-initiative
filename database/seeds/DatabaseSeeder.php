8984638e29170290c91c6c1d0a8208c0b6f3034e
agoujil saad <agoujilsaad@MacBook-Pro-de-pro-2.local>
2017-08-29 13:54:58 +0100

    typo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         
+        $this->call(UsersTableSeeder::class);
         $this->call(ProjetsTableSeeder::class);
         $this->call(EvenementsTableSeeder::class);
         $this->call(PartenairesTableSeeder::class);
