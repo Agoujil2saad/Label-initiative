dc5d5d607842ba6ce756fbf603a64a3c095e5ee2
agoujil saad <agoujilsaad@MacBook-Pro-de-pro-2.local>
2017-09-11 20:16:28 +0100

    just somme tweaks testing onesignal

--- resources/views/partials/one_signal_notification.blade.php
+++ resources/views/partials/one_signal_notification.blade.php
@@ -5,12 +5,11 @@ var OneSignal = window.OneSignal || [];
 OneSignal.push(["init", 
 						{
 							appId: "fde5427c-6447-4dc6-ae18-528489e21018",
-							autoRegister: true,
+							autoRegister: false,
 							notifyButton: 
 							{
 								enable: true /* Set to false to hide */
 							},
-
 							welcomeNotification: 
 							{
 								"title": "Label initiative",
@@ -30,18 +29,7 @@ OneSignal.push(["init",
 								{
         							enable: true, /* Required to use the notify button */
         							size: 'medium', /* One of 'small', 'medium', or 'large' */
-        							colors: { // Customize the colors of the main button and dialog popup button
-      											'circle.background': 'rgb(255,47,36)',
-      											'circle.foreground': 'white',
-      											'badge.background': 'rgb(255,47,36)',
-      											'badge.foreground': 'white',
-      											'badge.bordercolor': 'white',
-      											'pulse.color': 'white',
-      											'dialog.button.background.hovering': 'rgb(77, 101, 113)',
-      											'dialog.button.background.active': 'rgb(70, 92, 103)',
-      											'dialog.button.background': 'rgb(255,47,36)',
-      											'dialog.button.foreground': 'white'
-    										},
+        							theme: 'inverse', /* One of 'default' (red-white) or 'inverse" (white-red) */
         							position: 'bottom-left', /* Either 'bottom-left' or 'bottom-right' */
         							offset: 
         								{
